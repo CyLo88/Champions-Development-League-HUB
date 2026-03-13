6009343a81196bba08735800a426f725fa1aa9e8
CyLo88 <cylokuan@yahoo.com>
2026-03-13 17:09:13 -0400

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-
-async function fetchJSON(url){
- try{let r=await fetch(url);return await r.json();}catch{return null;}
-}
-async function init(){
- let data = await fetchJSON("https://api.sleeper.app/v1/league/1253490267638464512/rosters");
- document.getElementById("championsStandings").innerText = data ? "Data Loaded" : "Unable to load data";
-}
-init();
